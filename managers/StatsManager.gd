e95af360dace0cc588ce1b3344e2ccb12a6517e8
Piotr Jurga <piotr.j.jurga@gmail.com>
2026-07-24 13:26:20 +0200

    basic stats tracking

---
+++ managers/StatsManager.gd
@@ -0,0 +1,54 @@
+extends Node
+
+var current_army_id = 1
+var army = []
+
+func army_count():
+    return army.size()
+
+func fork_count():
+    var result = 0
+    for a in army: result += a.fork
+    return result
+
+func armor_count():
+    var result = 0
+    for a in army: result += a.armor
+    return result
+
+func speed_count():
+    var result = 0
+    for a in army: result += (a.speed > 1)
+    return result
+
+func add_minion(n = 1):
+    for i in range(n):
+        army.push_back({id=current_army_id, fork=false, armor=false, speed=1})
+        current_army_id += 1
+
+func get_fork():
+    var unarmed_minions = army.filter(func(a): return !a.fork)
+    if unarmed_minions.size() > 0:
+        var m = unarmed_minions.pick_random()
+        m.fork = true
+
+func get_armor():
+    var unarmed_minions = army.filter(func(a): return !a.armor)
+    if unarmed_minions.size() > 0:
+        var m = unarmed_minions.pick_random()
+        m.armor = true
+
+func get_speed():
+    var slow_minions = army.filter(func(a): return a.speed == 1)
+    if slow_minions.size() > 0:
+        var m = slow_minions.pick_random()
+        m.speed = 2
+
+func remove_minion(id):
+    var idx = army.find_custom(func(a): return a.id == id)
+    if idx >= 0:
+        army.pop_at(idx)
+
+func _ready():
+    add_minion(3)
+    Signals.minion_died.connect(remove_minion)
--- scenes/bullet-hell/bullet_hell.gd
+++ scenes/bullet-hell/bullet_hell.gd
@@ -5,14 +5,12 @@ var enemy : PackedScene = preload("res://scenes/bullet-hell/enemies/enemy.tscn")
 var minion : PackedScene = preload("res://scenes/bullet-hell/minions/minion.tscn")
 var player : PackedScene = preload("res://scenes/bullet-hell/player.tscn")
 
-var minion_count: int
 var enemy_count: int
 var last_tick_count : int = 0
 var max_last_tick_count : int = 2
 
-func minion_down():
-    minion_count -= 1
-    if minion_count == 0:
+func minion_down(_id):
+    if StatsManager.army.is_empty():
         Signals.scene_done.emit(false)
 
 func enemy_down():
@@ -21,21 +19,31 @@ func enemy_down():
         Signals.scene_done.emit(true)
 
 func next_scene() -> PackedScene:
-    if minion_count >= 1:
-        return ScenesManager.guillotine
-    return ScenesManager.town
+    if StatsManager.army.is_empty():
+        return ScenesManager.town # TODO(piotr): game over
+    return ScenesManager.guillotine
 
-func setup(enemy_count_: int, minion_count_: int):
-    minion_count = minion_count_
+func setup(enemy_count_: int):
     enemy_count = enemy_count_
     var p = player.instantiate()
     p.position = Vector2(window_size.x*0.5, window_size.y*0.8)
     add_child(p)
 
-    var radius = 50.0 * minion_count
+    var minion_count = StatsManager.army.size()
+    var radius = 30.0 * minion_count
     var angle_delta = 2*PI / minion_count
-    for i in range(minion_count):
+    for i in range(StatsManager.army.size()):
         var m = minion.instantiate()
+
+        var stats = StatsManager.army[i]
+        if stats.fork:
+            pass # TODO
+        if stats.armor:
+            pass # TODO
+        m.speed *= stats.speed
+        m.id = stats.id
+
+        # TODO(piotr): hex grid? triangle?
         var angle = i * angle_delta
         var offset = radius * Vector2(cos(angle), sin(angle))
         m.position = p.position + offset
@@ -54,7 +62,7 @@ func setup(enemy_count_: int, minion_count_: int):
 func _ready():
     super._ready()
     window_size = get_window().size
-    setup(5, 5)
+    setup(5)
     Signals.minion_died.connect(minion_down)
     Signals.enemy_died.connect(enemy_down)
 
